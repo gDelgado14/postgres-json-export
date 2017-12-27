2a10b9a6315debf10e76d552d04b51417f07bfaa
Giorgio Delgado <hi@gdelgado.ca>
2017-12-27 10:55:46 -0500

    first commit

---
+++ dbRunner.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+
+const toJsonQuery = async (tableName, pool) => {
+  result = await pool.query(`SELECT row_to_json(${tableName}) FROM ${tableName}`)
+
+  return {
+    table_name: tableName,
+    result
+  }
+}
+
+const writeFileToDisk = ({ table_name, result }) => {
+  const jsonResult = normalizeRows(result)
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(`./tables/${table_name}.json`, jsonResult, 'utf8', (err) => {
+      if (err) return reject(err)
+
+      resolve()
+    })
+  })
+}
+
+const normalizeRows = (tableResult) => JSON.stringify(tableResult.rows.map(row => row.row_to_json))
+
+module.exports = async (pool) => {
+  const res = await pool.query(`
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema='public' AND table_type='BASE TABLE';`
+  )
+
+  const promises = res.rows.map((r) => toJsonQuery(r.table_name, pool))
+
+  const jsonResult = await Promise.all(promises)
+
+  await Promise.all(jsonResult.map(writeFileToDisk))
+
+  console.log('done')
+
+  pool.end()
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const { Pool, Client } = require('pg')
+
+const dbRunner = require('./dbRunner')
+
+const {
+  PGUSER,
+  PGPASSWORD,
+  PGHOST,
+  PGDATABASE,
+  PGPORT
+} = process.env
+
+// pools will use environment variables
+// for connection information
+const pool = new Pool({
+  user: PGUSER,
+  host: PGHOST,
+  database: PGDATABASE,
+  password: PGPASSWORD,
+  port: PGPORT
+})
+
+dbRunner(pool)
